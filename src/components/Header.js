25282c430d32131da722486a6e602a93facebf88
Teki <68933219+secondtime-github@users.noreply.github.com>
2023-06-01 01:22:03 +0900

    add Semantic HTML

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from "../images/Logo.svg"
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="logo" />
+        </header>
+    );
+};
+
+export default Header;
